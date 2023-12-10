e2484cfa8c09126fa8e87fc9c56331c7c7a07926
David Raab <davidraab83@gmail.com>
2023-12-10 12:53:02 +0100

    Some more Ideas

---
+++ lib/Core/Measure.pm
@@ -0,0 +1,12 @@
+=head1 Units of Measure
+
+Some implementaion / library to not just have plain int/floats. Instead
+have real numbers like meters, kilometers, temperature, size, ...
+
+and functions to convert and operate on them in a safe manner.
+
+=cut
+
+package Measure;
+
+1;
