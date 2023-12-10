707a7553336f2c39479d0a642ca9e9535d168e57
David Raab <davidraab83@gmail.com>
2023-12-10 14:06:46 +0100

    Updated Project
    + MANIFEST, META
    + Moved some files

--- lib/Sq.pm
+++ lib/Sq.pm
@@ -1,105 +1,11 @@
 package Sq;
-
 use 5.036;
-use strict;
-use warnings;
+our $VERSION = '0.006';
+
+1;
 
 =head1 NAME
 
-Sq - The great new Sq!
-
-=head1 VERSION
-
-Version 0.01
+Sq - The Sq language
 
 =cut
-
-our $VERSION = '0.01';
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
-    use Sq;
-
-    my $foo = Sq->new();
-    ...
-
-=head1 EXPORT
-
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
-
-=head1 SUBROUTINES/METHODS
-
-=head2 function1
-
-=cut
-
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-David Raab, C<< <davidraab83 at gmail.com> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-sq at rt.cpan.org>, or through
-the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=Sq>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Sq
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=Sq>
-
-=item * CPAN Ratings
-
-L<https://cpanratings.perl.org/d/Sq>
-
-=item * Search CPAN
-
-L<https://metacpan.org/release/Sq>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-This software is Copyright (c) 2023 by David Raab.
-
-This is free software, licensed under:
-
-  The MIT (X11) License
-
-
-=cut
-
-1; # End of Sq
