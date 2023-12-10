40c163f26099829b6196026ca4b7f184fd894bb6
David Raab <davidraab83@gmail.com>
2023-12-10 15:23:00 +0100

    The Sq language

--- lib/Sq.pm
+++ lib/Sq.pm
@@ -33,4 +33,31 @@ sub assign :prototype(&) {
 
 Sq - The Sq language
 
-=cut
+=head1 SYNOPSIS
+
+What is a programming language? The foundation of every programming language
+are the data-structures the language provides you by default. The default
+data-structures and their possibilites shape how you will build a solution.
+
+Sq is a module that provides certain data-structures. Those data-structures
+are meant as a replacement for the built-in Perl data-structures. But replacing
+those data-structures will change the way how you code.
+
+Sq provides an immutable lazy sequence as its core data-structure (Seq),
+an immutable list (List), mutable Array (Array) and a mutable Queue (Queue).
+
+Those data-structures will share the same API as much as possible.
+
+Besides the above it will also provide a (Record) as an immutable Hash and
+Discriminated Unions.
+
+Instead of classes, typing is done with an Structural Typing approach. This
+can be used as an argument validator or even as a testing tool.
+
+Sq don't invent new syntax, it just uses Perl as much as possible. It
+is also implemented in pure Perl so far.
+
+Besides just data-structures the idea of Sq is to have a certain high-level
+API for basic programming tasks. An easier API for doing all kinds of
+File System operations or reading and writing from files like CSV or JSON that
+uses the data-structures of this module.
