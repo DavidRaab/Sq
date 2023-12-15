07a2392587591fb56ea25e967767fa91927ce9ff
David Raab <davidraab83@gmail.com>
2023-12-15 16:04:44 +0100

    Renamed files

--- examples/AoC-2023/01p1.pl
+++ examples/AoC-2023/01p1.pl
@@ -6,8 +6,16 @@ use Sq;
 
 # https://adventofcode.com/2023/day/1
 
+my ($opt, $usage) = describe_options(
+    'Usage: %c %o',
+    ['file|f=s', 'file to work with',  {required     => 1}],
+    ['help|h',   'Print this message', {shortcircuit => 1}],
+);
+
+$usage->die if $opt->help;
+
 my $sum =
-    Sq->io->open_text("01.txt")
+    Sq->io->open_text($opt->file)
     ->str_split(qr//)
     ->map( sub($array)    { Array::filter($array, \&is_num)       })
     ->map( sub($array)    { [ $array->first(0), $array->last(0) ] })
@@ -16,5 +24,3 @@ my $sum =
     ->sum;
 
 printf "Sum: %d\n", $sum;
-
-# $file->iter(sub($line) { print $line });
