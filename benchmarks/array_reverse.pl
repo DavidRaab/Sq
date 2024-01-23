028059028273e83e367f98154d53d285395c8853
David Raab <davidraab83@gmail.com>
2024-01-23 15:42:08 +0100

    Added done_testing()

--- benchmarks/array_reverse.pl
+++ benchmarks/array_reverse.pl
@@ -18,7 +18,7 @@ my $a1 = [1..100];
 my $a2 = [1..10_000];
 my $a3 = [1..100_000];
 
-# check if it is realle the same
+# check if it is really the same
 is(rev($a1), [reverse @$a1], 'rev 1');
 is(rev($a2), [reverse @$a2], 'rev 2');
 is(rev($a3), [reverse @$a3], 'rev 3');
@@ -27,6 +27,8 @@ is(rev($a3), [reverse @$a3], 'rev 3');
 is($a1, [1..100],     'same 1');
 is($a2, [1..10_000],  'same 2');
 is($a3, [1..100_000], 'same 3');
+
+done_testing();
 
 cmpthese(-1, {
     'own_implementation' => sub {
