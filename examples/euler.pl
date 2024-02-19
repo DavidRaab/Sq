97b08014f3f60a4b70518a9913abeab4d9f973f1
David Raab <davidraab83@gmail.com>
2024-02-19 17:41:32 +0100

    Example computing Euler

---
+++ examples/euler.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+use v5.36;
+use open ':std', ':encoding(UTF-8)';
+use Sq;
+
+# 1 + (1/1) + (1 / 1*2) + (1 / 1*2*3) + (1 / 1*2*3*4) + ...
+my $euler_seq = Seq->unfold(0, sub($i){ return (1/fac($i)), $i+1 });
+
+for my $iteration ( 1 .. 20 ) {
+    my $euler = $euler_seq->take($iteration)->sum;
+    printf "%2d -> %.15f\n", $iteration, $euler;
+}
+
+sub fac($n) {
+    return 1 if $n == 0;
+    my $result = 1;
+    $result *= $_ for 1 .. $n;
+    return $result;
+}
