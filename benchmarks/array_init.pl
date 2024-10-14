54b42105ad981e571bc4ac86c473ae04e3410c31
David Raab <davidraab83@gmail.com>
2024-10-14 21:28:50 +0200

    Removed unneeded stuff

--- benchmarks/array_init.pl
+++ benchmarks/array_init.pl
@@ -1,18 +1,8 @@
 #!/usr/bin/env perl
 use v5.36;
 use open ':std', ':encoding(UTF-8)';
-use Data::Printer;
-use Getopt::Long::Descriptive;
 use Sq;
 use Benchmark qw(cmpthese);
-
-my ($opt, $usage) = describe_options(
-    'Usage: %c %o',
-    ['help|h', 'Print this message', {shortcircuit => 1}],
-);
-
-$usage->die if $opt->help;
-
 
 sub init_seq($count, $f) {
     local $_;
