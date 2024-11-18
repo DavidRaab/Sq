002705c3f82a0a89758220f3ee23867e092f5a55
David Raab <davidraab83@gmail.com>
2024-11-18 03:34:47 +0100

    Nothing is free

---
+++ examples/moose.pl
@@ -0,0 +1,104 @@
+#!/usr/bin/env perl
+use v5.36;
+use open ':std', ':encoding(UTF-8)';
+use Sq;
+use Benchmark qw(cmpthese);
+
+package Movie;
+use Moose;
+
+has 'title'  => ( is => 'rw', isa => 'Str' );
+has 'rating' => ( is => 'rw', isa => 'Int' );
+has 'desc'   => ( is => 'rw', isa => 'Str' );
+
+__PACKAGE__->meta->make_immutable;
+
+package MoviePP;
+
+sub new($class, %args) {
+    return bless({
+        title  => $args{title},
+        rating => $args{rating},
+        desc   => $args{desc},
+    }, $class);
+}
+
+sub title($self, $title=undef) {
+    if ( defined $title ) {
+        $self->{title} = $title;
+        return;
+    }
+    else {
+        return $self->{title};
+    }
+}
+
+sub rating($self, $rating=undef) {
+    if ( defined $rating ) {
+        $self->{rating} = $rating;
+        return;
+    }
+    else {
+        return $self->{rating};
+    }
+}
+
+sub desc($self, $desc=undef) {
+    if ( defined $desc ) {
+        $self->{desc} = $desc;
+        return;
+    }
+    else {
+        return $self->{desc};
+    }
+}
+
+package main;
+
+cmpthese(-1, {
+    moose => sub {
+        for ( 1 .. 1_000 ) {
+            my $m = Movie->new(
+                title  => 'Terminator 2',
+                rating => 5,
+                desc   => 'Awesome',
+            );
+        }
+    },
+    sq_new => sub {
+        for ( 1 .. 1_000 ) {
+            my $m = Hash->new(
+                title  => 'Terminator 2',
+                rating => 5,
+                desc   => 'Awesome',
+            );
+        }
+    },
+    sq_bless => sub {
+        for ( 1 .. 1_000 ) {
+            my $m = Hash->bless({
+                title  => 'Terminator 2',
+                rating => 5,
+                desc   => 'Awesome',
+            });
+        }
+    },
+    perl_class => sub {
+        for ( 1 ..  1_000 ) {
+            my $m = MoviePP->new(
+                title  => 'Terminator 2',
+                rating => 5,
+                desc   => 'Awesome',
+            );
+        }
+    },
+    perl_hash => sub {
+        for ( 1 .. 1_000 ) {
+            my $m = {
+                title  => 'Terminator 2',
+                rating => 5,
+                desc   => 'Awesome',
+            };
+        }
+    }
+});
