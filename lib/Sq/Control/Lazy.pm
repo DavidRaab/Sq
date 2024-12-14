967d0b0bf027b43b54b89cf03d884e5d959ee2d1
David Raab <davidraab83@gmail.com>
2024-12-14 22:43:51 +0100

    Free sub-ref when executed once

--- lib/Sq/Control/Lazy.pm
+++ lib/Sq/Control/Lazy.pm
@@ -7,7 +7,12 @@ sub lazy :prototype(&) {
 
 sub force($self) {
     return $self->[1] if @$self == 2;
-    return $self->[1] = $self->[0]();
+    # execute function and save result
+    $self->[1] = $self->[0]();
+    # delete function from slot 0. This frees the subroutine and all it's
+    # memory/closure attached to it.
+    $self->[0] = 0;
+    return $self->[1];
 }
 
 1;
