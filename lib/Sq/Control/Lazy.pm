b058e52183ada7fc9300dcdd3e0d155b3f7bf11d
David Raab <davidraab83@gmail.com>
2024-12-25 15:26:48 +0100

    Lazy: Changed internal representation
    Now Lazy is a subroutine-reference. Why change the internal representation?
    Because this way lazy {} can be potential used whenever a subroutine
    is expected.

--- lib/Sq/Control/Lazy.pm
+++ lib/Sq/Control/Lazy.pm
@@ -2,17 +2,18 @@ package Sq::Control::Lazy;
 use 5.036;
 
 sub lazy :prototype(&) {
-    return bless([$_[0]], 'Sq::Control::Lazy');
+    my ( $fn ) = @_;
+    my $cache;
+    return bless(sub {
+        return $cache if !defined $fn;
+        $cache = $fn->();
+        $fn    = undef;
+        return $cache;
+    }, 'Sq::Control::Lazy');
 }
 
 sub force($self) {
-    return $self->[1] if @$self >= 2;
-    # execute function and save result
-    $self->[1] = $self->[0]();
-    # delete function from slot 0. This frees the subroutine and all it's
-    # memory/closure attached to it.
-    $self->[0] = 0;
-    return $self->[1];
+    return $self->();
 }
 
 1;
