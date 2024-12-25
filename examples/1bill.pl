72b80ff4ab537acfeb7b379dda819dbd0f0461f6
David Raab <davidraab83@gmail.com>
2024-12-25 18:09:55 +0100

    Added running time to example

--- examples/1bill.pl
+++ examples/1bill.pl
@@ -3,12 +3,16 @@ use 5.036;
 use open ':std', ':encoding(UTF-8)';
 use Sq;
 use Sq::Sig;
+use Time::HiRes qw(time);
 
 my $first  =
     Seq
     ->range(1,1_000_000_000)
     ->do(sub($num){ print "$num\n" if $num % 100_000 == 0 });
-my $second = Seq->range(1,1_000_000_000);
+my $second =
+    Seq->range(1,1_000_000_000);
+
+my $start = time();
 
 print "Are those sequences lazy?\n";
 if ( equal($first,$second) ) {
@@ -17,3 +21,6 @@ if ( equal($first,$second) ) {
 else {
     print "No!\n";
 }
+
+my $stop = time();
+printf "Time took: %f seconds\n", ($stop - $start);
