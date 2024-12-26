503e35ef256da2106e54860c65ab14151d55c111
David Raab <davidraab83@gmail.com>
2024-12-26 21:21:09 +0100

    Updated Benchmark

--- benchmarks/queue_size_array.pl
+++ benchmarks/queue_size_array.pl
@@ -8,14 +8,15 @@ use Sq::Test;
 use Benchmark qw(cmpthese);
 use Devel::Size qw(total_size);
 
-my @queue = 1 .. 100_000;
+my @queue = ();
 printf "Queue Size: %d\n", (total_size(\@queue));
 
-for ( 1 .. 100_000 ) {
+for ( 1 .. 1_000_000 ) {
     push @queue, 1, 2;
     shift @queue;
     shift @queue;
-    if ( $_ % 1000 == 0 ) {
+    if ( $_ % 100_000 == 0 ) {
         printf "Queue Size: %d\n", (total_size \@queue);
     }
 }
+
