d4a265134517d82d9bd4d26f00f3f4db8d0fd22f
David Raab <davidraab83@gmail.com>
2024-12-26 18:05:31 +0100

    An Exporter
    An exporter/importer that allows to load a module and specify with `-as`
    at which package the function should be imported.
    
    Used it for a short time in Collections but don't use it at the moment
    anymore.

--- lib/Sq/Exporter.pm
+++ lib/Sq/Exporter.pm
@@ -1,6 +1,50 @@
 package Sq::Exporter;
 use 5.036;
+use Sq::Reflection;
+no strict 'refs'; ## no critic
+sub import {
+    my ($pkg) = caller;
+    *{$pkg . '::' . 'import'} = \&export_import;
+}
 
-# Simple Exporter that maybe fits the need of Sq better
+sub export_import($own, @args) {
+    my %opt;
+    my @requested;
+
+    # process input. put key => value where key starts with `-` into
+    # %opt. All other things into @requested
+    my $idx = 0;
+    while ( $idx < @args ) {
+        my $name = $args[$idx];
+        if ( substr($name, 0, 1) eq '-' ) {
+            $opt{$name} = $args[$idx+1];
+            $idx += 2;
+        }
+        else {
+            push @requested, $name;
+            $idx++;
+        }
+    }
+
+    # Export functions into target defined with '-as'
+    if ( defined $opt{-as} ) {
+        my $target = $opt{-as};
+
+        # when no function requested then just export all functions in current
+        # package into target package
+        if ( @requested == 0 ) {
+            for my $func ( Sq::Reflection::all_funcs($own) ) {
+                *{$target . '::' . $func} = \&{$own . '::' . $func};
+            }
+        }
+        # otherwise just export requested
+        else {
+            for my $func ( @requested ) {
+                *{$target . '::' . $func} = \&{$own . '::' . $func};
+            }
+        }
+    }
+    return;
+}
 
 1;
