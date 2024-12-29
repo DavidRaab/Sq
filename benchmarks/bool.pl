e044390b71ca5e7f19b44fc54f41f20be004cab3
David Raab <davidraab83@gmail.com>
2024-12-29 16:09:35 +0100

    Updated micro benchmark

--- benchmarks/bool.pl
+++ benchmarks/bool.pl
@@ -2,9 +2,8 @@
 use 5.036;
 use open ':std', ':encoding(UTF-8)';
 use Sq;
-use Sq::Sig;
-use Sq::Type;
-use Sq::Test;
+# use Sq::Sig;
+# use Sq::Test;
 use Benchmark qw(cmpthese);
 
 my @inputs = (0, 10, "foo", -1, undef, [], 1, {});
@@ -37,8 +36,18 @@ cmpthese(-1, {
             $count++ if
                 defined $in
                 && length($in) == 1
-                && Scalar::Util::looks_like_number($in)
+                && is_num($in)
                 && ($in == 0 || $in == 1);
+        }
+        die "\$count not 2\n" if $count != 2;
+    }},
+    length_regex => sub {for ( 1 .. 1_000 ) {
+        my $count = 0;
+        for my $in ( @inputs ) {
+            $count++ if
+                defined $in
+                && length($in) == 1
+                && $in =~ m/\A[01]\z/;
         }
         die "\$count not 2\n" if $count != 2;
     }},
@@ -47,7 +56,7 @@ cmpthese(-1, {
         for my $in ( @inputs ) {
             $count++ if
                 ref $in eq ""
-                && Scalar::Util::looks_like_number($in)
+                && is_num($in)
                 && ($in == 0 || $in == 1);
         }
         die "\$count not 2\n" if $count != 2;
