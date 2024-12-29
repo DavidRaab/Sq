928304a42fee003006898903a6d7b32ac01b0b85
David Raab <davidraab83@gmail.com>
2024-12-29 18:50:57 +0100

    Example to check missing functions

--- examples/check_funcs.pl
+++ examples/check_funcs.pl
@@ -5,16 +5,23 @@ use Sq;
 use Sq::Reflection;
 use Sq::Sig;
 
+my %seq_skip   = map { $_ => 1 } qw/bless blit pop push shift shuffle unshift map_e filter_e/;
+my %array_skip = map { $_ => 1 } qw/always cache do doi infinity/;
+
 my $array = all_funcs('Array');
 my $seq   = all_funcs('Seq');
 
 print "Array is missing following Array functions.\n";
-Hash::difference($seq->count, $array->count)->iter_sort(by_str, sub($key,$) {
-    printf "  + %s\n", $key;
+Hash::difference($seq->count, $array->count)->keys->remove(sub($func) {
+    $array_skip{$func}
+})->sort(by_str)->iter(sub($func) {
+    printf "  + %s\n", $func;
 });
 
 print "\nSeq is missing following Array functions.\n";
-Hash::difference($array->count, $seq->count)->iter_sort(by_str, sub($key,$) {
-    printf "  + %s\n", $key;
+Hash::difference($array->count, $seq->count)->keys->remove(sub($func) {
+    $seq_skip{$func}
+})->sort(by_str)->iter(sub($func) {
+    printf "  + %s\n", $func;
 });
 
