d405ffa1d0cc2aac5e7f356fd25dbe5ab81e65a0
David Raab <davidraab83@gmail.com>
2024-12-30 23:29:26 +0100

    Still running

---
+++ examples/1bill_slower.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/env perl
+use 5.036;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Sq;
+use Sq::Sig;
+
+# functional-style
+my $countup = assign {
+    my $bill1   = Seq->range(1,1_000_000_000);
+    my $is_even = sub($x) { $x & 1 };
+
+    Seq::flatten_array(
+        Seq::zip(
+            Seq::filter($bill1, $is_even),
+            Seq::remove($bill1, $is_even),
+        )
+    );
+};
+
+run(sub {
+    $countup->iter(sub($x){
+        if ( $x % 10_000 == 0 ) {
+            print "First: $x\n";
+        }
+    });
+});
+
+# procedural / oo-style
+my $bill1   = Seq->range(1,1_000_000_000);
+my $is_even = sub($x) { $x & 1 };
+my $evens   = $bill1->filter($is_even);
+my $unevens = $bill1->remove($is_even);
+my $zip     = $evens->zip($unevens);
+my $flatten = $zip->flatten_array;
+
+run(sub {
+    $flatten->iter(sub($x){
+        if ( $x % 10_000 == 0 ) {
+            print "Second: $x\n";
+        }
+    });
+});
+
+sub run($f) {
+    my $start = time();
+
+    $f->();
+
+    my $stop = time();
+    printf "Time: %d seconds\n", $stop;
+}
