d34340246e90747402e5ab359257383f10435386
David Raab <davidraab83@gmail.com>
2024-12-30 19:05:39 +0100

    Updated example all_funcs.pl

--- examples/all_funcs.pl
+++ examples/all_funcs.pl
@@ -1,12 +1,13 @@
 #!/usr/bin/env perl
 use 5.036;
+use utf8;
 use open ':std', ':encoding(UTF-8)';
 use Sq;
+use Sq::Reflection;
 use Sq::Sig;
 
 print "All defined Array functions:\n\n";
-my $count = 1;
-sq([Sq::Reflection::all_funcs('Array')])->sort(by_str)->chunked(10)->iter(sub($array) {
+all_funcs('Array')->sort(by_str)->chunked(10)->iter(sub($array) {
     print $array->join(', '), "\n";
 });
 print "\n";
