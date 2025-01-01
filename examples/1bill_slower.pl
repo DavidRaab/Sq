957e0f8db0af00ac69730b2098d5591e29bff638
David Raab <davidraab83@gmail.com>
2025-01-01 02:16:49 +0100

    Added comment to 1bill_slower.pl

--- examples/1bill_slower.pl
+++ examples/1bill_slower.pl
@@ -13,7 +13,7 @@ my $countup = assign {
     Seq::merge(
         # Hmm, i think Seq::zip should built a sequence too instead.
         # Or not? The reason behind this behavior:
-        # When zip runs, it defenitely must read on element from each list.
+        # When zip runs, it defenitely must read one element from each list.
         # (maybe in future from multiple list). So you have all data at once
         # in an array. It doesn't make sense to save it as a sequence again
         # and pretend it would be a sequence if it is not. That's why it stays
@@ -52,11 +52,18 @@ run(sub {
     });
 });
 
+# Basic concept of lambdas. You run some other code and maybe do something
+# before/after it. Instead of side-effects like here you also could process
+# the input, change/modify it, or do something on the return value. Nothing
+# special. In OO you achieve the same by creating a new class, in a new
+# file (because clean code tells you so, right?) and inherit from a base
+# class and override a method. Or other kind of boilerplate-code crap.
+# Seriously stop coding like that!
 sub run($f) {
     my $start = time();
 
     $f->();
 
     my $stop = time();
-    printf "Time: %d seconds\n", $stop;
+    printf "Time: %d seconds\n", ($stop-$start);
 }
