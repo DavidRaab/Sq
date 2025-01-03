3cb579af669755293bee4c2284660dba0cde3976
David Raab <davidraab83@gmail.com>
2025-01-03 15:18:05 +0100

    Added Signature Loading to Exporter

--- lib/Sq/Exporter.pm
+++ lib/Sq/Exporter.pm
@@ -2,7 +2,9 @@ package Sq::Exporter;
 use 5.036;
 use Carp ();
 
-no strict 'refs'; ## no critic
+no strict   'refs'; ## no critic
+no warnings 'once';
+
 sub import {
     my ($pkg) = caller;
     *{"$pkg\::import"} = \&export_import;
@@ -12,14 +14,16 @@ sub export_import($own, @args) {
     my ( $target ) = caller;
 
     # get our @export of current package
-    my $exports = \@{"$own\::EXPORT"};
+    my $exports   = \@{"$own\::EXPORT"};
+    # get signature
+    my $signature = ${"$own\::SIGNATURE"};
 
     # Build a list of options and commands that should be exported
     my %opt;
     my @cmds;
     my $idx = 0;
     while ( $idx < @args ) {
-        my $current = $args[0];
+        my $current = $args[$idx];
         if ( substr($current, 0, 1) eq '-' ) {
             $opt{$current} = $args[$idx+1];
             $idx += 2;
@@ -28,6 +32,14 @@ sub export_import($own, @args) {
             push @cmds, $current;
             $idx++;
         }
+    }
+
+    # Load default signature when -sig => 1 is provided and $SIGNATURE
+    # is defined in module
+    if ( $opt{-sig} && defined $signature ) {
+        local $@;
+        eval "require $signature";
+        Carp::croak $@ if $@;
     }
 
     # Export default
@@ -41,7 +53,7 @@ sub export_import($own, @args) {
     # Export requested
     else {
         my %ok = map { $_ => 1 } @$exports;
-        for my $func ( @args ) {
+        for my $func ( @cmds ) {
             my $fn = *{"$own\::$func"}{CODE};
             Carp::croak "function '$func' is not in \@EXPORT" if not $ok{$func};
             Carp::croak "function '$func' does not exists. Check \@EXPORT in $own" if !defined $fn;
--- t/10-exporter.t
+++ t/10-exporter.t
@@ -17,6 +17,15 @@ sub world() {
     return 'World';
 }
 
+package Test2;
+use Sq;
+use Sq::Exporter;
+our @EXPORT    = qw(foo bar);
+our $SIGNATURE = 'Sq::Sig::Array';
+
+sub foo() { ... }
+sub bar() { ... }
+
 package main;
 
 # Manual import of function
@@ -34,7 +43,9 @@ nok(has_func('Test', 'foo'),   'has_func 1');
  ok(has_func('Test', 'hello'), 'has_func 2');
 nok(has_func('main', 'hello'), 'has_func 3');
 
-Test->import('hello');
+# manual call to import()
+Test->import('hello', -sig => 1);
+
  ok(has_func('main', 'hello'), 'has_func 4');
 nok(has_func('main', 'world'), 'has_func 5');
 
@@ -49,5 +60,16 @@ like(
     dies { Test->import('test') },
     qr/\Afunction 'test' does not exists/,
     'function not in @EXPORT fails');
+
+# check if foo() and bar() is imported
+nok(has_func('main', 'foo'), 'foo not yet imported');
+nok(has_func('main', 'bar'), 'bar not yet imported');
+
+Test2->import(-sig => 1);
+
+ok(has_func('main', 'foo'), 'foo is imported');
+ok(has_func('main', 'bar'), 'bar is imported');
+
+# TODO: tests that really checks if signature was loaded???
 
 done_testing;
