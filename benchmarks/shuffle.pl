165debd596b5b9e3583c9f0d9b2dbed9327690d4
David Raab <davidraab83@gmail.com>
2025-01-04 17:27:02 +0100

    Shuffle Benchmarks

---
+++ benchmarks/shuffle.pl
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+use 5.036;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Sq;
+use Sq::Test;
+use Sq::Sig;
+use List::Util qw(shuffle);
+
+# swap every element with a random new one
+sub shuffle1($array) {
+    my @new = @$array;
+    my $max = @$array;
+    my ($new_idx, $tmp);
+    for ( my $idx=0; $idx < $max; $idx++ ) {
+        $new_idx       = rand($max);
+        $tmp           = $new[$idx];
+        $new[$idx]     = $new[$new_idx];
+        $new[$new_idx] = $tmp;
+    }
+    return bless(\@new, 'Array');
+}
+
+# from List::Util
+sub shuffle2($array) {
+    CORE::bless([List::Util::shuffle @$array], 'Array');
+}
+
+# shuffle like selection sort
+sub shuffle3($array) {
+    my @new  = @$array;
+    my $max  = @$array;
+    my ($tmp, $offset);
+    for ( my $idx=0; $idx < $max; $idx++ ) {
+        $offset        = rand($max - $idx);
+        $tmp           = $new[$idx];
+        $new[$idx]     = $new[$idx + $offset];
+        $new[$idx+$offset] = $tmp;
+    }
+    return bless(\@new, 'Array');
+}
+
+# my $data = [1..10];
+# for ( 1 .. 1_000 ) {
+#     dumpw(shuffle3($data));
+#     if ( Array::sum($data) != 55 ) {
+#         warn "Error: %s\n", dump($data);
+#     }
+# }
+
+# "shuffle2" here is ~60% faster compared to "current". Why?
+# Array::shuffle is exactly implemented as shuffle2().
+
+my $array = Array->range(1,100);
+Sq->bench->compare(-1, {
+    current  => sub {
+        for ( 1 .. 1_000 ) {
+            my $s = Array::shuffle($array);
+        }
+    },
+    shuffle1 => sub {
+        for ( 1 .. 1_000 ) {
+            my $s = shuffle1($array)
+        }
+    },
+    shuffle2 => sub {
+        for ( 1 .. 1_000 ) {
+            my $s = shuffle2($array)
+        }
+    },
+    shuffle3 => sub {
+        for ( 1 .. 1_000 ) {
+            my $s = shuffle3($array)
+        }
+    },
+});
