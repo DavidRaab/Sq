2bfe31d1ab412e80e95f17df8bf864576ffaebfa
David Raab <davidraab83@gmail.com>
2025-01-05 11:31:54 +0100

    Added: funcs_wo_sig.pl
    List all functions that is missing a signature.

---
+++ examples/funcs_wo_sig.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+use 5.036;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Sq;
+use Sq::Reflection;
+use Sq::Gen;
+use Sq::Parser;
+use Sq::Sig;
+
+# This script prints all functions in Sq, that have no type-check with
+# signature added.
+
+sub fqdn($package) {
+    my %skip;
+    for my $name ( @Sq::EXPORT ) {
+        $skip{$name} = 1 if is_str($name);
+    }
+    $skip{import} = 1;
+
+    all_funcs($package)
+    ->remove(sub($name) { $skip{$name}    })
+    ->rxs(qr/\A/, sub   { $package . '::' });
+}
+
+my $funcs = Array->concat(
+    fqdn('Array'),  fqdn('Seq'),        fqdn('Option'),
+    fqdn('Result'), fqdn('Sq::Parser'), fqdn('Sq::Gen'),
+);
+
+# dumpw($funcs);
+
+my $sigs = Sq::Signature::sigs_added();
+
+# dumpw($sigs);
+
+say "Following functions is missing a signature:";
+Array::diff($funcs, $sigs, \&id)->iter_sort(by_str, sub($func) {
+    say $func;
+});
