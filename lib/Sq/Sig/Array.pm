b0704e73c9a928cc68f0b7a6fd52d79031644485
David Raab <davidraab83@gmail.com>
2025-01-12 16:32:35 +0100

    Changed: Array::split signature
    the built-in `split` in Perl takes a regex. It is good practice in Perl to
    always pass a regex to split and not a string. For example `split '.', @array`
    probably does not what you want.
    
    It does not split something on a dot. It splits something on the first
    character. as this is what `.` means in a regex. To avoid this common
    mistakes where someone provides a string without realizing the string is
    interpreted as a regex, now you must pass a regex, otherwise it is a
    failure.
    
    Run into the same problem even with knowing. So now i change that.

--- lib/Sq/Sig/Array.pm
+++ lib/Sq/Sig/Array.pm
@@ -122,7 +122,7 @@ sig('Array::last',       $array,                       $opt);
 sig('Array::sum',        $anum,                        t_num);
 sig('Array::sum_by',     $array, $sub,                 t_num);
 sig('Array::join',       $astr, t_str,                 t_str);
-sig('Array::split',      $array, t_or(t_regex, t_str), t_array(t_of $astr));
+sig('Array::split',      $astr, t_regex,              t_array(t_of $astr));
 sigt('Array::min',
     t_or(t_tuple($anum), t_tuple($anum, t_any)),
     t_or(t_opt(t_num), t_num));
--- t/Signature/01-Array.t
+++ t/Signature/01-Array.t
@@ -27,6 +27,7 @@ like(dies { sq([[], [], 1])->to_array_of_array  }, qr/\AArray::/, 'to_array_of_a
 like(dies { Array->range(1,10)->windowed("foo") }, qr/\AArray::/, 'windowed');
 like(dies { sq([1,2,3])->repeat("foo")          }, qr/\AArray::/, 'repeat');
 like(dies { Array::fill2d([{}], sub {})         }, qr/\AArray::/, 'fill2d');
+like(dies { Array::split(["foo+bar"], '+')      }, qr/\AArray::split:/, 'split');
 
 like(dies { sq(["12-12", "10-10", []])->rxm(qr/\A(\d\d)-(\d\d)\z/) }, qr/\AArray::/, 'rxm');
 done_testing;
