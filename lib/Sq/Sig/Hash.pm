643a15f47e298a1b495346abdba89b365e2fc53c
David Raab <davidraab83@gmail.com>
2025-01-12 22:37:32 +0100

    Refactored Signature
    This can potential be a little bit faster and consuming less memory.
    When you write `t_hash` then it is a function that creates a single
    anonymous function that checks for a hash. So calling it ~60 times
    also creates 60 of those functions. But as those functions are pure
    they can be easily shared. Only one is needed.

--- lib/Sq/Sig/Hash.pm
+++ lib/Sq/Sig/Hash.pm
@@ -5,70 +5,75 @@ use Sq::Type;
 use Sq::Signature;
 
 # Some predefined types
-my $kv = t_tuple(t_str, t_any);
+my $any  = t_any();
+my $kv   = t_tuple(t_str, $any);
+my $hash = t_hash();
+my $sub  = t_sub();
+my $int  = t_int();
 
 ### CONSTRUCTORS
 
-sig ('Hash::empty',      t_any,                                  t_hash);
-sigt('Hash::new',        t_tuplev(t_any, t_array(t_even_sized)), t_hash);
-sig ('Hash::bless',      t_any, t_hash,                          t_hash);
-sig ('Hash::locked',     t_any, t_hash,                          t_hash);
-sig ('Hash::init',       t_any, t_int, t_sub,                    t_hash);
-sig ('Hash::from_array', t_any, t_array, t_sub,                  t_hash);
+sig ('Hash::empty',      $any,                                  $hash);
+sigt('Hash::new',        t_tuplev($any, t_array(t_even_sized)), $hash);
+sig ('Hash::bless',      $any, $hash,                           $hash);
+sig ('Hash::locked',     $any, $hash,                           $hash);
+sig ('Hash::init',       $any, $int, $sub,                    $hash);
+sig ('Hash::from_array', $any, t_array, $sub,                  $hash);
 
 ### METHODS
 
-sig ('Hash::keys',         t_hash,                t_array(t_of t_str));
-sig ('Hash::values',       t_hash,                t_array);
-sig ('Hash::map',          t_hash, t_sub,         t_hash);
-sig ('Hash::find',         t_hash, t_sub,         t_opt($kv));
-sig ('Hash::pick',         t_hash, t_sub,         t_opt);
-sig ('Hash::keep',         t_hash, t_sub,         t_hash);
-sig ('Hash::fold',         t_hash, t_any, t_sub,  t_any);
-sig ('Hash::length',       t_hash,                t_int);
-sig ('Hash::is_empty',     t_hash,                t_bool);
-sig ('Hash::bind',         t_hash, t_sub,         t_hash);
-sig ('Hash::append',       t_hash, t_hash,        t_hash);
-sig ('Hash::union',        t_hash, t_hash, t_sub, t_hash);
-sig ('Hash::intersection', t_hash, t_hash, t_sub, t_hash);
-sig ('Hash::diff',         t_hash, t_hash,        t_hash);
-sigt('Hash::concat',       t_tuplev(t_hash, t_array(t_of t_hash)), t_hash);
-sig ('Hash::is_subset_of', t_hash, t_hash,        t_int);
-sig ('Hash::get',          t_hash, t_str,         t_opt);
-sig ('Hash::copy',         t_hash,                t_hash);
-sigt('Hash::extract',      t_tuplev(t_hash, t_array(t_min(1), t_of t_str)), t_array(t_of t_opt));
-sigt('Hash::slice',        t_tuplev(t_hash, t_array(t_min(1), t_of t_str)), t_hash);
-sigt('Hash::with',         t_tuplev(t_hash, t_array(t_even_sized)),         t_hash); # can be improved
-sigt('Hash::withf',        t_tuplev(t_hash, t_array(t_even_sized)),         t_hash); # can be improved
-sigt('Hash::has_keys',     t_tuplev(t_hash, t_array(t_of t_str)),           t_bool);
-sig ('Hash::equal',        t_hash, t_any,                                   t_bool);
-sig ('Hash::to_array',     t_hash, t_sub,                                   t_array);
+sig ('Hash::keys',         $hash,                t_array(t_of t_str));
+sig ('Hash::values',       $hash,                t_array);
+sig ('Hash::map',          $hash, $sub,          $hash);
+sig ('Hash::find',         $hash, $sub,          t_opt($kv));
+sig ('Hash::pick',         $hash, $sub,          t_opt);
+sig ('Hash::keep',         $hash, $sub,          $hash);
+sig ('Hash::fold',         $hash, $any, $sub,    $any);
+sig ('Hash::fold_back',    $hash, $any, $sub,    $any);
+sig ('Hash::length',       $hash,                $int);
+sig ('Hash::is_empty',     $hash,                t_bool);
+sig ('Hash::bind',         $hash, $sub,          $hash);
+sig ('Hash::append',       $hash, $hash,         $hash);
+sig ('Hash::union',        $hash, $hash, $sub,   $hash);
+sig ('Hash::intersection', $hash, $hash, $sub,   $hash);
+sig ('Hash::diff',         $hash, $hash,         $hash);
+sigt('Hash::concat',       t_tuplev($hash, t_array(t_of $hash)), $hash);
+sig ('Hash::is_subset_of', $hash, $hash,         $int);
+sig ('Hash::get',          $hash, t_str,         t_opt);
+sig ('Hash::copy',         $hash,                $hash);
+sigt('Hash::extract',      t_tuplev($hash, t_array(t_min(1), t_of t_str)), t_array(t_of t_opt));
+sigt('Hash::slice',        t_tuplev($hash, t_array(t_min(1), t_of t_str)), $hash);
+sigt('Hash::with',         t_tuplev($hash, t_array(t_even_sized)),         $hash); # can be improved
+sigt('Hash::withf',        t_tuplev($hash, t_array(t_even_sized)),         $hash); # can be improved
+sigt('Hash::has_keys',     t_tuplev($hash, t_array(t_of t_str)),           t_bool);
+sig ('Hash::equal',        $hash, $any,                                    t_bool);
+sig ('Hash::to_array',     $hash, $sub,                                    t_array);
 
 ### SIDE-EFFECTS
 
-sigt('Hash::on',      t_tuplev(t_hash, t_array(t_even_sized)), t_void);
-sig ('Hash::iter',    t_hash, t_sub,                           t_void);
-sigt('Hash::lock',    t_tuplev(t_hash, t_array(t_of t_str)),   t_hash);
+sigt('Hash::on',      t_tuplev($hash, t_array(t_even_sized)), t_void);
+sig ('Hash::iter',    $hash, $sub,                            t_void);
+sigt('Hash::lock',    t_tuplev($hash, t_array(t_of t_str)),   $hash);
 
 ### MUTATION METHODS
 
-sigt('Hash::set',     t_tuplev(t_hash, t_array(t_even_sized)),         t_void);
-sigt('Hash::change',  t_tuplev(t_hash, t_array(t_even_sized)),         t_void);
-sigt('Hash::push',    t_tuplev(t_hash, t_str, t_array(t_min 1)),       t_void);
-sigt('Hash::delete',  t_tuplev(t_hash, t_array(t_min(1), t_of t_str)), t_void);
+sigt('Hash::set',     t_tuplev($hash, t_array(t_even_sized)),         t_void);
+sigt('Hash::change',  t_tuplev($hash, t_array(t_even_sized)),         t_void);
+sigt('Hash::push',    t_tuplev($hash, t_str, t_array(t_min 1)),       t_void);
+sigt('Hash::delete',  t_tuplev($hash, t_array(t_min(1), t_of t_str)), t_void);
 
 sigt('Hash::dump',
     t_or(
-        t_tuple(t_hash),
-        t_tuple(t_hash, t_int),
+        t_tuple($hash),
+        t_tuple($hash, $int),
     ),
     t_str
 );
 
 sigt('Hash::dumpw',
     t_or(
-        t_tuple(t_hash),
-        t_tuple(t_hash, t_int),
+        t_tuple($hash),
+        t_tuple($hash, $int),
     ),
     t_void
 );
