e2eef938253ece54e11089980606c8cbad7560e3
David Raab <davidraab83@gmail.com>
2025-01-14 18:18:12 +0100

    Updated examples/funcs_array.pl
    Used Table Format and Array::columns() to print functions.

--- examples/funcs_array.pl
+++ examples/funcs_array.pl
@@ -3,11 +3,9 @@ use 5.036;
 use utf8;
 use open ':std', ':encoding(UTF-8)';
 use Sq;
-use Sq::Reflection;
 use Sq::Sig;
 
 print "All defined Array functions:\n\n";
-all_funcs('Array')->sort(by_str)->chunked(10)->iter(sub($array) {
-    print $array->join(', '), "\n";
+Sq->fmt->table({
+    data => Sq::Reflection::funcs_of('Array')->sort(by_str)->columns(7),
 });
-print "\n";
