eb1ae15cd4732573de7b0e6a5ac498b07f2487a0
David Raab <davidraab83@gmail.com>
2025-01-14 19:33:01 +0100

    Added: Example: To show statics and which have no signature.

---
+++ examples/funcs_statics.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+use 5.036;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Sq;
+use Sq::Reflection qw(statics signatures);
+use Sq::Sig;
+
+print "Currently defined statics.\n";
+my $statics = statics();
+Sq->fmt->table({
+    data => $statics->sort(by_str)->columns(4),
+});
+print "\n";
+
+my $missing = Array::diff($statics, signatures(), \&id);
+if ( @$missing ) {
+    print "Following statics are missing a signature.\n";
+    say $missing->sort(by_str)->join(', ');
+}
+else {
+    print "All statics have a siganture.\n";
+}
