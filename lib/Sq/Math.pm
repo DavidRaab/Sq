1adb0ad6283315c0fb557b29340cea9183226b17
David Raab <davidraab83@gmail.com>
2025-01-15 18:24:32 +0100

    doc: Updated Sq::Manual::Concepts::Statics

--- lib/Sq/Math.pm
+++ lib/Sq/Math.pm
@@ -2,7 +2,7 @@ package Sq::Math;
 use 5.036;
 use Sq qw(static);
 
-static 'is_prime', sub($x) {
+static is_prime => sub($x) {
     state @primes = (
         2,   3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
         59, 61, 67, 71, 73, 79, 83, 89, 97, 101
