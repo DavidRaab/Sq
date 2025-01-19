185d5c8d325dfe2dda272c960546a778cb425ff8
David Raab <davidraab83@gmail.com>
2025-01-19 18:49:15 +0100

    Updated Permutations Examples

--- examples/permutation.pl
+++ examples/permutation.pl
@@ -2,13 +2,23 @@
 use v5.36;
 use open ':std', ':encoding(UTF-8)';
 use Sq;
+#use Sq::Sig;
 
+# permute() is now part of Array/Seq. Use this one as it is much faster.
+# See "examples/permute.pl"
+#
+# Runtime:
+# examples/permutation.pl -> ~300 secs
+# examples/permute.pl     -> ~17 secs
+#
+# This stays as an example as it still shows the laziness of a Sequence.
+#
 # This is definetly not the fastest way to get all possible permutations.
 # The point is, that this code still works. computes/prints all permutation
-# and starts immidiately without waiting or creating memory problems.
+# and starts immediately without waiting or creating memory problems.
 
 # keys for permutation
-my $keys = Seq->wrap(qw/A B C D E F G H I J/);
+my $keys = seq { qw/A B C D E F G H I J/ };
 
 # only picks permutation so far
 my $permutation = sub ($x) {
@@ -31,11 +41,14 @@ my $permut =
 
 # Start printing permutations
 my $count = 0;
-$permut->iter(sub ($x) {
-    $count++;
-    print $x, "\n";
+Sq->bench->it(sub {
+    $permut->iter(sub ($x) {
+        $count++;
+        print $x, "\n";
+    });
 });
 
+printf "Permutation Should be: %d\n", Sq->math->fac(10);
 printf "Permutation Count: %d\n", $count;
 
 # check if string has duplicate characters
--- examples/permute.pl
+++ examples/permute.pl
@@ -3,7 +3,15 @@ use 5.036;
 use utf8;
 use open ':std', ':encoding(UTF-8)';
 use Sq;
-use Sq::Test;
-use Sq::Sig;
+# use Sq::Sig;
 
-dump(array(qw/A B C D/)->permute);
+my $count = 0;
+Sq->bench->it(sub {
+    seq { qw/A B C D E F G H I J/ }->permute->iter(sub($array) {
+        $count++;
+        say $array->join("");
+    });
+});
+
+say "Count should be: ", Sq->math->fac(10);
+say "Count: $count";
