97712bc027a976d744c0968c179857d3e243e500
David Raab <davidraab83@gmail.com>
2025-01-20 14:47:50 +0100

    Added comment

--- lib/Sq/Math.pm
+++ lib/Sq/Math.pm
@@ -40,9 +40,32 @@ static fac => sub($num) {
     return $result;
 };
 
-# Expects an array of int
-# For example starts with:
-# [0,0,0,0]
+# Expects an array of int. You always starts with an array with everything
+# being zero. Like: [0,0,0,0]. Needs at least two [0,0] to work correctly.
+#
+### Description of how it works:
+# The integer represents the indexes of an array you must remove from left
+# to right to generate a permutation. For example [0,0,0,0] means you must
+# remove four times in a row the first element to generate the first permutation.
+#
+# ['A', 'B', 'C', 'D'] -> []                   # remove 0
+# ['B', 'C', 'D']      -> ['A']                # remove 0
+# ['C', 'D']           -> ['A', 'B']           # remove 0
+# ['D']                -> ['A', 'B', 'C']      # remove 0
+# []                   -> ['A', 'B', 'C', 'D']
+#
+# This functions is basically just a counting algorithm. But instead that every
+# digit has the same base, the base increases. The most right one always must
+# be '0'. Then the 2nd last element either can be 0,1, then third last can be
+# 0,1,2 and so on. The maximum value for 4 int is: [3,2,1,0]
+# For 5 ints it is: [4,3,2,1,0]
+#
+# This function just counts the array up, and modify it's value (mutable). But
+# then is used to generate a permutation. This way all permutations can be lazily
+# generated as only the current "count-value" must be kept in memory.
+#
+# You also can read it up in Higher-Order Perl (You can get a free pdf copy
+# on the website!)
 static permute_count_up => sub($array) {
     my $idx = $array->$#* - 1;
     my $max = @$array - ($idx+1);
