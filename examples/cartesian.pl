a4f033f3c9a29731ead90bdd60f195afb12ac6db
David Raab <davidraab83@gmail.com>
2025-01-26 15:49:47 +0100

    Made cartesian example a litte bit more useful

--- examples/cartesian.pl
+++ examples/cartesian.pl
@@ -3,6 +3,10 @@ use 5.036;
 use utf8;
 use open ':std', ':encoding(UTF-8)';
 use Sq -sig => 1;
-# use Sq::Test;
 
-dump(Array::cartesian([1..10], [qw/J Q K A/], [qw/C G T/]));
+Sq->fmt->table({
+    data =>
+        Array::cartesian([1..10], [qw/J Q K A/], [qw/C G T/])
+        ->map(call 'join', ',')
+        ->columns(12),
+});
