d4a32f13efdc35ec2e35c5022bb40249945c3e5e
David Raab <davidraab83@gmail.com>
2025-01-26 15:18:57 +0100

    Made count_up state of cartesian()

--- examples/cartesian.pl
+++ examples/cartesian.pl
@@ -5,25 +5,24 @@ use open ':std', ':encoding(UTF-8)';
 use Sq -sig => 1;
 use Sq::Test;
 
-sub count_up($max, $counter) {
-    my $idx = $counter->$#*;
-
-    UP:
-    $counter->[$idx]++;
-    if ( $counter->[$idx] >= $max->[$idx] ) {
-        $counter->[$idx] = 0;
-        $idx--;
-        return 0 if $idx < 0;
-        goto UP;
-    }
-    return 1;
-};
-
 sub cartesian(@arrays) {
+    state $count_up = sub($max, $counter) {
+        my $idx = $counter->$#*;
+        UP:
+        $counter->[$idx]++;
+        if ( $counter->[$idx] >= $max->[$idx] ) {
+            $counter->[$idx] = 0;
+            $idx--;
+            return 0 if $idx < 0;
+            goto UP;
+        }
+        return 1;
+    };
+
     my $maxs    = Array::map(\@arrays, call 'length');
     my $counter = [(0) x (scalar @arrays)];
     my @new     = [map { $_->[0] } @arrays];
-    while ( count_up($maxs, $counter) ) {
+    while ( $count_up->($maxs, $counter) ) {
         my @inner;
         for (my $aidx=0; $aidx < @arrays; $aidx++) {
             push @inner, $arrays[$aidx][$counter->[$aidx]];
