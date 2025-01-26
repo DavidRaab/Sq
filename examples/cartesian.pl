ac040ad2b832761dc3f94f43137e8ffd71da9024
David Raab <davidraab83@gmail.com>
2025-01-26 15:17:48 +0100

    Example: cartesian on variable amount of arrays

---
+++ examples/cartesian.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+use 5.036;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Sq -sig => 1;
+use Sq::Test;
+
+sub count_up($max, $counter) {
+    my $idx = $counter->$#*;
+
+    UP:
+    $counter->[$idx]++;
+    if ( $counter->[$idx] >= $max->[$idx] ) {
+        $counter->[$idx] = 0;
+        $idx--;
+        return 0 if $idx < 0;
+        goto UP;
+    }
+    return 1;
+};
+
+sub cartesian(@arrays) {
+    my $maxs    = Array::map(\@arrays, call 'length');
+    my $counter = [(0) x (scalar @arrays)];
+    my @new     = [map { $_->[0] } @arrays];
+    while ( count_up($maxs, $counter) ) {
+        my @inner;
+        for (my $aidx=0; $aidx < @arrays; $aidx++) {
+            push @inner, $arrays[$aidx][$counter->[$aidx]];
+        }
+        push @new, CORE::bless(\@inner, 'Array');
+    }
+    return CORE::bless(\@new, 'Array');
+}
+
+dump(cartesian([1..10], [qw/J Q K A/], [qw/C G T/]));
