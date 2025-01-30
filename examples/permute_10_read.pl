701702e32db11745fe6c5176f86a1a8cf7e0bec7
David Raab <davidraab83@gmail.com>
2025-01-30 18:14:20 +0100

    Updated Examples

--- examples/permute_10_read.pl
+++ examples/permute_10_read.pl
@@ -5,13 +5,14 @@ use open ':std', ':encoding(UTF-8)';
 use Sq;
 # use Sq -sig => 1;
 
-if ( !-e "permute_10.txt.gz" ) {
-    print "Run 'write_permute.pl' first.\n";
+if ( !-e "permute_10.cache.gz" ) {
+    print "Run 'permute_10_write.pl' first.\n";
+    exit;
 }
 
 Sq->bench->it(sub {
     my @permute = ('A' .. 'J');
-    Sq->fs->read_text_gz('permute_10.txt.gz')->split(qr/,/)->iter(sub($array) {
+    Sq->fs->read_text_gz('permute_10.cache.gz')->split(qr/,/)->iter(sub($array) {
         print @permute[@$array], "\n";
     });
 });
--- examples/permute_10_write.pl
+++ examples/permute_10_write.pl
@@ -5,8 +5,8 @@ use open ':std', ':encoding(UTF-8)';
 use Sq -sig => 1;
 # use Sq::Test;
 
-print "Generating file 'permute_10.txt.gz'\n";
+print "Generating file 'permute_10.cache.gz'\n";
 Sq->bench->it(sub {
     my $permute = seq { 0 .. 9 }->permute->map(call 'join', ',');
-    Sq->fs->write_text_gz('permute_10.txt.gz', $permute);
+    Sq->fs->write_text_gz('permute_10.cache.gz', $permute);
 });
