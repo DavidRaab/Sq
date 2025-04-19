3d5e7b67e19a08d5c7ca82946b5fc755c2c5ac63
David Raab <davidraab83@gmail.com>
2025-04-19 14:34:09 +0200

    Added comments

--- benchmarks/is_prime.pl
+++ benchmarks/is_prime.pl
@@ -5,6 +5,7 @@ use open ':std', ':encoding(UTF-8)';
 use Sq -sig => 1;
 use Sq::Test;
 
+# This function is a copy of Sq->math->is_prime and is tested in Sq.
 sub is_prime_a($x) {
     state @primes = (
         2,   3,  5,  7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53,
@@ -68,7 +69,10 @@ sub is_prime_b($x) {
     return 1;
 };
 
-# Test if both functions are correct by checking that they return the same
+# Test if both functions are correct by checking that they return the same.
+# This also has another effect. The first run from 1 .. 100_000 fills the
+# state @primes array in both functions so when the Benchmark runs, it doesn't
+# need to build the @primes array anymore.
 my $failure;
 for my $x ( 1 .. 100_000 ) {
     if ( is_prime_a($x) != is_prime_b($x) ) {
@@ -77,6 +81,7 @@ for my $x ( 1 .. 100_000 ) {
     }
 }
 is($failure, undef, 'no failure');
+done_testing;
 
 # Benchmark
 my $max = 100_000;
