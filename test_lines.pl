f7007353a2c7311a2bc0a3cc9db758dd8395df49
David Raab <davidraab83@gmail.com>
2025-10-26 17:01:04 +0100

    Added script: test_lines.pl
    I have a lot of test file. But every test file actually causes the test-suite
    to run longer. Consider this. For every test file that is executed the
    whole Sq must be read, parsed and converted into an opcode then Perl
    executes that opcode. Perl is a Virtual Machine.
    
    For this it's still extremely fast. I didn't count the files but just
    loading Sq itself probably reads and loads 10-20 files so far.
    
    This is done for every test, and i have around 60 test files so far.
    Consider that the whole test suite still just runs in around 3 CPU seconds
    on my machine (Ryzen 5600, 6 Cores @ 4.4Ghz)
    
    Adding new test files isn't a problem. A simple "time perl -MSq -E1"
    show that it takes around 25ms to load the whole Sq system. So every
    test file has a constant overhead.
    
    For very short test files. The overhead is bigger than running the tests.
    
    So i have written a quick tool to see how much lines every test-file has.
    So i am able to merge some multiple test files into one big file. This
    make running the tests-suite faster.
    
    Still not much a problem at the moment, but the bigger the project goes,
    it needs some refactoring.
    
    Consider that before I used Sq::Test i used Test2. Test2 showed in profiling
    that 90%+ was consumed by Test2. Not even by Sq. When i switches from Test2,
    quite some time ago. It already needed 5 CPU seconds to run. And there
    i was at around just 2000 tests.
    
    Now i am at nearly 2800 tests. And went from 45 files to aroun 60 files,
    and i am still at just around 3 cpu seconds.
    
    Also consider. That F# just needs 3 seconds to start, to just print
    a "hello, world" to the screen. So it needs nearly 3 seconds to read, parse
    and compile a single line of code.
    
    I mean when F# itself runs, the code is usually faster than Perl. It also
    has a JIT, Perl don't. But starting/executing is a nightmare with F# or
    general C# and so on.
    
    I am used to work in a very quickly/iterative way. Do some changes and
    immediately run my code and the test-suite. It's sometimes a nightmare
    to work with F#. It feels like more waiting then writing code.

---
+++ test_lines.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+use 5.036;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+use Sq -sig => 1;
+
+print "This program just prints every test file and the amount of lines ",
+      "in every file.\n";
+
+my $files =
+    Sq->fs->recurse('t')->choose(sub($file) {
+        if ( -f $file && $file->basename =~ m/\.t$/ ) {
+            return Some [$file->stringify, Sq->fs->read_text($file)->length ]
+        }
+        else {
+            return None
+        }
+    })
+    ->sort_by(by_num, idx 1)
+    ->rev;
+
+Sq->fmt->table({
+    header => [qw/File Lines/],
+    data   => $files,
+    border => 0,
+});
+
+printf "\nTotal Test Lines: %d\n", $files->sum_by(idx 1);
