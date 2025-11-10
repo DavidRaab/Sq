28d471fd7cc7b61cc5432ca9b99f67106aecfa1f
David Raab <davidraab83@gmail.com>
2025-11-10 21:22:06 +0100

    Corrected signature

--- lib/Sq/Sig/Str.pm
+++ lib/Sq/Sig/Str.pm
@@ -9,6 +9,7 @@ my $num  = t_num;
 my $bool = t_bool;
 my $sub  = t_sub;
 my $pint = t_int(t_min 1);
+my $array = t_array;
 
 sig('Sq::Core::Str::length',      $str, t_int);
 sig('Sq::Core::Str::lc',          $str, $str);
@@ -27,12 +28,12 @@ sig('Sq::Core::Str::repeat',      $str, $num, $str);
 sig('Sq::Core::Str::starts_with', $str, $str, $bool);
 sig('Sq::Core::Str::ends_with',   $str, $str, $bool);
 sig('Sq::Core::Str::contains',    $str, $str, $bool);
-sig('Sq::Core::Str::chunk',       $str, $pint, t_array);
+sig('Sq::Core::Str::chunk',       $str, $pint, $array);
 sig('Sq::Core::Str::map',         $str, $sub,  $str);
 # sig('Sq::Core::Str::keep',        $str, $sub,  $str);
 sig('Sq::Core::Str::remove',      $str, $sub,  $str);
-sig('Sq::Core::Str::split',       $str, t_regex, $str);
-sig('Sq::Core::Str::to_array',    $str, t_array);
+sig('Sq::Core::Str::split',       t_regex, $str, $array);
+sig('Sq::Core::Str::to_array',    $str, $array);
 sig('Sq::Core::Str::is_empty',    $str, $str);
 
 1;
