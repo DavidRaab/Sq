749216c33859eed0d61744e564a26da23aaf20a6
David Raab <davidraab83@gmail.com>
2025-12-13 15:56:28 +0100

    Added another version using tr//

--- benchmarks/vowel_count.pl
+++ benchmarks/vowel_count.pl
@@ -64,8 +64,16 @@ sub vowel_regexi($text) {
     return scalar @matches;
 }
 
+# this uses tr//, it is specialiased to just replace characters. it
+# returns how many replacements are made. This avoid building an array
+# of matches
+sub vowel_tr($text) {
+    my $count = $text =~ tr/aeiouAEIOU/aeiouAEIOU/;
+    return $count;
+}
+
 my $words = Array::repeat(
-    [qw/The big brown fox jumps over the lazy Dog/],
+    [qw/The big brOwn fox jumps ovEr the lazy Dog/],
     100);
 
 Sq->bench->compare(-1, {
@@ -104,14 +112,22 @@ Sq->bench->compare(-1, {
         }
         die "\$count not correct" if $count != 1000;
     },
+    tr => sub {
+        my $count = 0;
+        for my $word ( @$words ) {
+            $count += vowel_tr($word);
+        }
+        die "\$count not correct" if $count != 1000;
+    },
 });
 
 __END__
 Result on my system.
 
-            Rate        cs cs_inline      hash    regexi     regex
-cs        78.3/s        --      -91%      -98%      -98%      -98%
-cs_inline  852/s      989%        --      -73%      -77%      -77%
-hash      3170/s     3948%      272%        --      -14%      -14%
-regexi    3691/s     4614%      333%       16%        --       -0%
-regex     3692/s     4615%      333%       16%        0%        --
+            Rate        cs cs_inline      hash     regex    regexi        tr
+cs        75.5/s        --      -91%      -97%      -98%      -98%      -99%
+cs_inline  852/s     1030%        --      -71%      -77%      -77%      -91%
+hash      2955/s     3816%      247%        --      -20%      -21%      -69%
+regex     3691/s     4792%      333%       25%        --       -1%      -61%
+regexi    3725/s     4837%      337%       26%        1%        --      -60%
+tr        9394/s    12351%     1002%      218%      155%      152%        --
