488a30dee39637f7af53b0d25eb258ae2a05e7ad
David Raab <davidraab83@gmail.com>
2025-12-21 21:22:20 +0100

    Correct Benchmarks can be hard

--- benchmarks/perl_signatures.pl
+++ benchmarks/perl_signatures.pl
@@ -9,7 +9,15 @@ sub add4 {
     my ($x, $y, $z, $w) = @_;
     $x + $y + $z + $w;
 }
+sub maps($array, $f) {
+    bless([map { $f->($_) } @$array], 'Array');
+}
+sub mapp {
+    my ($array, $f) = @_;
+    bless([map { $f->($_) } @$array], 'Array');
+}
 
+print "Benchmarking add4\n";
 my $nums = Sq->rand->int(1,1E6)->take(4 * 10_000)->chunked(4)->to_array;
 Sq->bench->compare(-1, {
     pure => sub {
@@ -24,7 +32,28 @@ Sq->bench->compare(-1, {
     }
 });
 
+print "\nBenchmarking map\n";
+my $add1 = sub { $_[0] + 1 };
+Sq->bench->compare(-1, {
+    mapp => sub {
+        for my $args ( @$nums ) {
+            mapp($args, $add1);
+        }
+    },
+    maps => sub {
+        for my $args ( @$nums ) {
+            maps($args, $add1);
+        }
+    }
+});
+
 __END__
+Benchmarking add4
            Rate signature      pure
-signature 710/s        --      -13%
-pure      815/s       15%        --
+signature 710/s        --      -14%
+pure      830/s       17%        --
+
+Benchmarking map
+      Rate maps mapp
+maps 171/s   --  -1%
+mapp 172/s   1%   --
