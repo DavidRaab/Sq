dcffa674487c84fe3ef8e95e3f0a1cd72af798e5
David Raab <davidraab83@gmail.com>
2025-12-21 21:42:16 +0100

    Update: Benchmarking can be hard

--- benchmarks/perl_signatures.pl
+++ benchmarks/perl_signatures.pl
@@ -4,6 +4,41 @@ use utf8;
 use open ':std', ':encoding(UTF-8)';
 use Sq; # -sig => 1;
 
+# This shows a classical synthetical Benchmark and why Benchmarking
+# can be hard.
+#
+# I wanted to know how much overhead Perl's signature feature. At least
+# the signature feature adds some runtime code to check it's arguments.
+# So i created two add4 versions. One with signature, the other without.
+#
+# It showed that the add4 version with signatures was about 15% faster.
+#
+# The wrong conclusion of this would be that signatures makes all your
+# function 15% slower. Understand it like this, just to pick some
+# imaginary numbers.
+#
+# with signature the signature maybe took 15ns, and adding 4 numbers
+# took 85ns. So you end up that the version with signatures is slower
+# around 15%.
+#
+# But rarely do we have such simple functions. When we have a function
+# like mapp() that creates a whole new array, goes through multiple values
+# and calls a function for each value. Well then maybe the function
+# took 10.000ns to run. And the signature still only took 15ns.
+#
+# So overall the performance of signature don't matter. This is a classical
+# case for "Premature Optimization". Not only that. Creating correct Benchmarks
+# is hard. As a general advice we always should create Benchmarks on "Real Data".
+#
+# It even can be that you provide code and optimize code that runs faster
+# against your synthetical benchmark, maybe you hardcoded your data and
+# you run a thousand times against the exact data. Only to find out that
+# when you run your code against real data you need to process it turns out
+# to be slower.
+#
+# So always be aware of Synthetical Benchmark. Correct Benchmarks can be
+# hard.
+
 sub add4s($x,$y,$z,$w) { $x + $y + $z + $w }
 sub add4 {
     my ($x, $y, $z, $w) = @_;
